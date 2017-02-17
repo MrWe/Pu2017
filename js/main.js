9f28afceaa6dbd0ff10393095e06917b7912301b
Håkon Halldal <haakhall@stud.ntnu.no>
2017-02-17 13:32:23 +0100

    laget skjelettet av siden

---
+++ js/main.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.6.9/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBnf39lchWQO2z7UPf9nrJfm_AN7Tpd8Dg",
+    authDomain: "aurora-80cde.firebaseapp.com",
+    databaseURL: "https://aurora-80cde.firebaseio.com",
+    storageBucket: "aurora-80cde.appspot.com",
+    messagingSenderId: "30087111932"
+  };
+  firebase.initializeApp(config);
+</script>
