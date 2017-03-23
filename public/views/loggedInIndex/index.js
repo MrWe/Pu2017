e71dd4c8e891080b5d1de499e45b632f097fad4d
mrwe <erikwik@stud.ntnu.no>
2017-03-23 10:48:03 +0100

    Lectures now shown on login

--- public/views/loggedInIndex/index.js
+++ public/views/loggedInIndex/index.js
@@ -1,3 +1,5 @@
+var lectures = {};
+var currentSelectedLecture = "";
 $(function() {
 
   hljs.configure({ // optionally configure hljs
@@ -15,7 +17,8 @@ $(function() {
   });
   quill.format('code-block', true);
 
-  $('#username').text(sessionStorage['currUser']);
+  $('#username')
+    .text(sessionStorage['currUser']);
 
   $("#logoutbtn")
     .click(function() {
@@ -26,10 +29,31 @@ $(function() {
     });
 
 
-    $('#submitbtn').click(function(){
+
+  $.post('/api/get_lectures', function(res) {
+    for (var i in res) {
+      console.log(i);
+      $('#lectures')
+        .append("<li class='lecture'><a  href='#'>" + i + "</a></li>")
+      lectures = res;
+
+    }
+    currentSelectedLecture = i;
+  });
+
+  $('#lectures')
+    .on('click', 'li.lecture', function(event) {
+      event.preventDefault();
+      currentSelectedLecture = lectures[event.target.text];
+    });
+
+  $('#submitbtn')
+    .click(function() {
       var t = quill.getText();
       t = t.replace(/\n$/, "")
-      $.post('/api/store_content', {scripts: t})
+      $.post('/api/store_content', {
+          scripts: t
+        })
         .done(function(res) {
           console.log("Submitted")
         });
